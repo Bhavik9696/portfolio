d19c52088c6ec15612ce7f14ebda181dc46a93c8
Bhavik 9696 <raibhavik7619325139@gmail.com>
2025-09-09 07:15:20 +0530

    add link for my aside logo

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -6,7 +6,7 @@ const Aside = () => {
     <div>
       <aside className="fixed right-4 top-64 flex flex-col space-y-6 z-50">
         <a 
-          href="https://wa.me/yourNumber" 
+          href="https://wa.me/qr/SPLZRPUSUPAWG1 " 
           target="_blank" 
           rel="noopener noreferrer"
           className="bg-green-500 text-white p-3 rounded-full shadow-lg hover:scale-110 transition-transform"
@@ -14,7 +14,7 @@ const Aside = () => {
           <FaWhatsapp size={22} />
         </a>
         <a 
-          href="https://t.me/yourUsername" 
+          href="https://t.me/+918431985374" 
           target="_blank" 
           rel="noopener noreferrer"
           className="bg-blue-500 text-white p-3 rounded-full shadow-lg hover:scale-110 transition-transform"
@@ -22,7 +22,7 @@ const Aside = () => {
           <FaTelegramPlane size={22} />
         </a>
         <a 
-          href="https://linkedin.com/in/yourProfile" 
+          href="https://www.linkedin.com/in/bhavik-rai-438a70294?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" 
           target="_blank" 
           rel="noopener noreferrer"
           className="bg-blue-700 text-white p-3 rounded-full shadow-lg hover:scale-110 transition-transform"
