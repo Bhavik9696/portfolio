b940128ad2142a64d7dde02203e308c729349f1e
Bhavik 9696 <raibhavik7619325139@gmail.com>
2025-10-04 09:09:11 +0530

    replace github link instend of teligram

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,38 +1,38 @@
-import React from 'react'
-import { FaWhatsapp, FaTelegramPlane, FaLinkedin } from "react-icons/fa";
+import React from "react";
+import { FaWhatsapp, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Aside = () => {
   return (
     <div>
       <aside className="fixed right-4 top-64 flex flex-col space-y-6 z-50">
-        <a 
-          href="https://wa.me/qr/SPLZRPUSUPAWG1 " 
-          target="_blank" 
+        <a
+          href="https://wa.me/qr/SPLZRPUSUPAWG1 "
+          target="_blank"
           rel="noopener noreferrer"
           className="bg-green-500 text-white p-3 rounded-full shadow-lg hover:scale-110 transition-transform"
         >
           <FaWhatsapp size={22} />
         </a>
-        <a 
-          href="https://t.me/+918431985374" 
-          target="_blank" 
+        <a
+          href="https://github.com/Bhavik9696"
+          target="_blank"
           rel="noopener noreferrer"
-          className="bg-blue-500 text-white p-3 rounded-full shadow-lg hover:scale-110 transition-transform"
+          className="bg-gray-800 text-white p-3 rounded-full shadow-lg hover:scale-110 transition-transform"
         >
-          <FaTelegramPlane size={22} />
+          <FaGithub size={22} />
         </a>
-        <a 
-          href="https://www.linkedin.com/in/bhavik-rai-438a70294?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" 
-          target="_blank" 
+
+        <a
+          href="https://www.linkedin.com/in/bhavik-rai-438a70294?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+          target="_blank"
           rel="noopener noreferrer"
           className="bg-blue-700 text-white p-3 rounded-full shadow-lg hover:scale-110 transition-transform"
         >
           <FaLinkedin size={22} />
         </a>
       </aside>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Aside
+export default Aside;
