0aa95896d1505a37ef178589ec5f0c1c9f1342d3
Bhavik 9696 <raibhavik7619325139@gmail.com>
2025-10-05 08:22:43 +0530

    replace updated resume

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({btntext}) => {
+const Button = ({ btntext }) => {
   return (
     <div>
       <a
-          href="/Bhavik_Rai_Resume.pdf" 
-          download="Bhavik_Rai_Resume.pdf"
-          className=" bg-purple-500 hover:bg-green-600 text-white font-medium px-6 py-3 rounded-xl shadow-md transition duration-300 mt-9"
-        >
-         {btntext}
-        </a>
-      
+        href="/myresume.pdf"
+        download="Bhavik_Rai_Resume.pdf"
+        className=" bg-purple-500 hover:bg-green-600 text-white font-medium px-6 py-3 rounded-xl shadow-md transition duration-300 mt-9"
+      >
+        {btntext}
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
